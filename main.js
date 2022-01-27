d2808d2173f97e0a61d35cd72cf107706a599ca8
David <50714711+schnavy@users.noreply.github.com>
2022-01-27 16:19:10 +0100

    styling + monil

--- main.js
+++ main.js
@@ -4,13 +4,12 @@ const IS_TOUCH = 'ontouchstart' in window || navigator.msMaxTouchPoints > 0
 let text = [
   'Independent Publishing Fair Leipzig, March 19, 2022.',
   '<span class="layer2">It’s a book</span>, it’s a word that you choose, it’s a language in use.',
-  'Opening hours 12 pm – 9 pm, symposium 1 pm – 6 pm.',
-  'Please be welcome to visit our marketplace and symposium at the <span class="layer2">Academy of Fine Arts Leipzig, Wächterstraße 11, 04107 Leipzig.</span> Join the digital event right here soon! (<a target="_blank"  href="https://itsabook.de">www.itsabook.de.</a>)',
-  'Submit your books until <span class="layer2">February 14</span>. Publishers to <a class="mail" href="mailto:publishers@itsabook.de">publishers@itsabook.de</a> and students to <a class="mail" href="mailto:students@itsabook.de">students@itsabook.de</a>.',
-  'Kindly supported by Support-Büro der HGB Leipzig and DZA Druckerei zu Altenburg.',
-  'Follow us on <a target="_blank" href="https://www.instagram.com/itsabook_leipzig/">Instagram</a> to stay updated.'
+  'Opening hours 12&nbsp;pm&nbsp;–&nbsp;9&nbsp;pm, symposium 1&nbsp;pm&nbsp;–&nbsp;6&nbsp;pm.',
+  'Please be welcome to visit our marketplace and symposium at the <span class="layer2">Academy of Fine Arts Leipzig, Wächterstraße 11, 04107 Leipzig.</span> Join the digital event right here! (<a target="_blank"  href="https://itsabook.de">www.itsabook.de</a>).',
+  'Submit your books until <span class="layer2">February 14</span>. Publishers to <a class="mail" href="mailto:publishers@itsabook.de">publishers@itsabook.de</a> and&nbsp;students&nbsp;to <a class="mail" href="mailto:students@itsabook.de">students@itsabook.de</a>.',
+  'Kindly supported by Support-Büro der HGB Leipzig and DZA Druckerei zu Altenburg.'
 ]
-let textElements = [];
+let textElements = []
 let container = document.querySelector('.container')
 let mainText = document.querySelector('.mainText')
 
@@ -36,43 +35,59 @@ text.forEach(t => {
   spacer.classList.add('spacer')
 
   text.innerHTML = t + ' '
-  spacer.innerHTML = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'
+  spacer.innerHTML = '&nbsp;'
   mainText.appendChild(text)
   mainText.appendChild(spacer)
-  textElements.push(text);
+  textElements.push(text)
+})
 
-  if (!IS_TOUCH) {
+if (!IS_TOUCH) {
+  textElements.forEach(text => {
     text.addEventListener('mouseenter', e => {
       if (!text.classList.contains('hover-style-1')) {
         toggleHightlight(text)
+      }else{
+        hightlightRandomSchnipsel()
       }
+
     })
     text.addEventListener('mouseleave', e => {
       toggleHightlight(text)
     })
-  }
-})
-if (IS_TOUCH) {
-  window.addEventListener('touchstart', e => {
-    console.log(e)
-    if (e.target.tagName.toLowerCase() === 'span') {
-      toggleHightlight(e.target)
-    }
   })
 }
+if (IS_TOUCH) {
+  // window.addEventListener('touchend', e => {
+  //   console.log(e)
+  //   if (e.target.tagName.toLowerCase() === 'span') {
+  //     toggleHightlight(e.target)
+  //   }
+  // })
 
-toggleHightlight(textElements[Math.floor(Math.random()*text.length)])
-toggleHightlight(textElements[Math.floor(Math.random() * text.length)])
-
-
-
+  let counter = textElements.length - 1
+  setInterval(e => {
+    if (counter % 2 == 0) {
+      hightlightRandomSchnipsel()
+    }
+    removeAllHighlights(textElements[counter])
+    if (counter >= textElements.length - 1) {
+      counter = 0
+    } else {
+      counter++
+    }
+    removeAllHighlights(textElements[counter])
+    toggleHightlight(textElements[counter])
+  }, 1200)
+} else {
+  toggleHightlight(textElements[Math.floor(Math.random() * text.length)])
+  toggleHightlight(textElements[Math.floor(Math.random() * text.length)])
+}
 
 function toggleHightlight (elem) {
   let hoverStyle = 'hover-style-' + Math.floor(Math.random() * 2 + 1)
 
   if (!elem.classList.contains('hover')) {
     let r = Math.floor(Math.random() * colors.length)
-    console.log(colors.length)
     elem.classList.add(hoverStyle)
     elem.classList.add('hover')
     elem.style.backgroundColor = colors[r]
@@ -93,9 +108,10 @@ function hightlightRandomSchnipsel () {
     elem.classList.remove('hover-style-1')
   })
   let r = Math.floor(Math.random() * schnipsel.length)
-  console.log(schnipsel[r])
-  let rc = Math.floor(Math.random() * colors.length)
 
-  schnipsel[r].classList.add("hover-style-1'")
-  elem.style.backgroundColor = colors[rc]
+  let rc = Math.floor(Math.random() * colors.length)
+  let rs = Math.floor(Math.random() * 2) + 1
+
+  schnipsel[r].classList.add('hover-style-' + rs)
+  schnipsel[r].style.backgroundColor = colors[rc]
 }
